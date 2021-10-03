ca2e37827a1bff8257f54d24e7c1e74e06325c5f
toddaulwurm <85901690+toddaulwurm@users.noreply.github.com>
2021-10-03 17:06:40 -0500

    dtrh

---
+++ Models/User.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+namespace FormSubmission.Models
+{
+    public class User
+    {
+        [Required (ErrorMessage ="First Name Needed!")]
+        [MinLength(4, ErrorMessage ="Needs between 4 and 50 characters!")]
+        public string FirstName{get;set;}
+
+        [Required (ErrorMessage ="Last Name Needed!")]
+        [MinLength(4,ErrorMessage ="Needs between 4 and 50 characters!")]
+        public string LastName{get;set;}
+
+        [Required (ErrorMessage ="Age Needed!")]
+        [Range(0,150, ErrorMessage ="Needs number between 0 and 150!")]
+        public int Age{get;set;}
+
+        [Required (ErrorMessage ="Email Needed!")]
+        [MinLength(4,ErrorMessage ="Needs between 4 and 50 characters!")]
+        public string Email{get;set;}
+
+        [Required (ErrorMessage ="Password Needed!")]
+        [MinLength(4,ErrorMessage ="Needs between 4 and 50 characters!")]
+        public string Password{get;set;}
+    }
+}
